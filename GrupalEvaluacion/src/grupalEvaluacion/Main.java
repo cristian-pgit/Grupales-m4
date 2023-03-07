45f64b9e76d14219d31c5cf5ba1a0a3ff73c8ec6
Cristian Carrillo <naitsircl@gmail.com>
2023-03-07 13:48:08 -0300

    update Grupal 07/03

--- GrupalEvaluacion/src/grupalEvaluacion/Main.java
+++ GrupalEvaluacion/src/grupalEvaluacion/Main.java
@@ -4,8 +4,78 @@ import java.util.Scanner;
 
 public class Main {
 	
+	public static Tablero board = new Tablero();
+	
+	public static void main (String[]Args) {
+		board.generarTablero();
+		board.genenrartableroH();
+		board.victroria();
+		
+				
+		board.ubicarKromis(board.tablero);
+		board.ubicarCaguano(board.tablero);
+		board.ubicarTrupalla(board.tablero);
+
+
+		Scanner sc = new Scanner(System.in);
+		boolean keepPlaying = true;
+		while (keepPlaying) {
+			System.out.println();
+			System.out.println(ANSI_GREEN+"Bientenido al Menu de <<Oeuf les flics>>"+"\tTu Puntaje: "+ANSI_RESET+ANSI_CYAN+board.puntaje+ANSI_GREEN);
+			System.out.println("Ingrese una opción:\n"
+					+ "\t1. Lanzar Huevo\n"
+					+ "\t2. Lanzar Huevo a la suerte\n"
+					+ "\t3. Mostrar los carros de la PKS\n"
+					+ "\t4. Ver info de los Carros PKS\n"
+					+ "\t5. Ver los Huevos lanzados\n"
+					+ "\t6. Salir"+ANSI_RESET);
+			int opcion = sc.nextInt();
+			switch (opcion) {
+			case 1:
+				Tablero.lanzarHuevo(board.tablero, board.tableroH, sc);
+				break;
+			case 2:
+				Tablero.takeALuckyShot(board.tablero, board.tableroH);
+				break;
+			case 3:
+				Tablero.mostrarTableroPks(board.tablero);
+				break;
+			case 4:
+				Tablero.mostrarDatos();
+				break;
+			case 5:
+				Tablero.mostrarHuevos(board.tablero);
+				break;
+			case 6:
+				keepPlaying = false;
+				System.out.println("Hasta una proxima ocasion...");
+				break;
+			case 0:
+				board.puntaje +=10;
+			}
+			if (board.puntaje >= 77) {
+				System.out.println(board.victroria()); 
+				keepPlaying = false;
+			}
+		}
+
+		sc.close();
+	}
 		
 
 	
+	
+	
+	
+	
+	
+	public static final String ANSI_WHITE = "\u001B[37m";
+	public static final String ANSI_PURPLE = "\u001B[35m";
+	public static final String ANSI_BLUE = "\u001B[34m";
+	public static final String ANSI_CYAN = "\u001B[36m";
+	public static final String ANSI_GREEN = "\u001B[32m";
+	public static final String ANSI_YELLOW = "\u001B[33m";
+	public static final String ANSI_RESET = "\u001B[0m";
+	public static final String ANSI_RED = "\u001B[31m";	   
 	
 }
--- GrupalEvaluacion/src/grupalEvaluacion/Tablero.java
+++ GrupalEvaluacion/src/grupalEvaluacion/Tablero.java
@@ -11,72 +11,43 @@ public class Tablero {
 		private static List<Carro> pks = new ArrayList<Carro>(18);
 		private static  List<Huevo> huevos = new ArrayList<Huevo>();
 		static Random random = new Random();
-		private static  int puntaje = 0;
+		public static  int puntaje = 0;
 		static Scanner scanner = new Scanner(System.in);
+		
 		
 		//Grilla de tablero
 	
-		public static void main (String[]Args) {
-			
-			String[][] tablero = new String[15][15];
-			for (int i = 0; i < 15; i++) {
-	            for (int j = 0; j < 15; j++) {
-	                tablero[i][j] = "[-]"; // inicializar y dejar en valor blanc en ves que esten null.
-	            }
-			}
-			
-			ubicarKromis(tablero);
-			ubicarCaguano(tablero);
-			ubicarTrupalla(tablero);
-			String[][] tableroH = new String[15][15];
-			for (int i = 0; i < 15; i++) {
-	            for (int j = 0; j < 15; j++) {
-	                tableroH[i][j] = "[-]"; // inicializar y dejar en valor blanc en ves que esten null.
-	            }
-			}
 		
-			Scanner sc = new Scanner(System.in);
-			boolean keepPlaying = true;
-	    	while (keepPlaying) {
-	    		System.out.println();
-	    		System.out.println(ANSI_GREEN+"Bientenido al Menu de <<Oeuf les flics>>"+"\tTu Puntaje: "+ANSI_RESET+ANSI_CYAN+puntaje+ANSI_GREEN);
-	    		System.out.println("Ingrese una opción:\n"
-	                + "\t1. Lanzar Huevo\n"
-	                + "\t2. Lanzar Huevo a la suerte\n"
-	                + "\t3. Mostrar los carros de la PKS\n"
-	                + "\t4. Ver info de los Carros PKS\n"
-	                + "\t5. Ver los Huevos lanzados\n"
-	                + "\t6. Salir"+ANSI_RESET);
-	        int opcion = sc.nextInt();
-	        switch (opcion) {
-	            case 1:
-	            	lanzarHuevo(tablero, tableroH, scanner);
-	                break;
-	            case 2:
-	            	takeALuckyShot(tablero, tableroH);
-	                break;
-	            case 3:
-	            	mostrarTableroPks(tablero);
-	                break;
-	            case 4:
-	            	mostrarDatos();
-					break;
-	            case 5:
-	                mostrarHuevos(tablero);
-					break;
-	            case 6:
-	                keepPlaying = false;
-	                System.out.println("Hasta una proxima ocasion...");
-					break;
+			
+		public String[][] tablero = new String[15][15];
+		public void generarTablero() {
+			for (int i = 0; i < 15; i++) {
+				for (int j = 0; j < 15; j++) {
+					tablero[i][j] = "[-]"; // inicializar y dejar en valor blanc en ves que esten null.
+				}
 			}
 		}
-	    
-			sc.close();
+			
+
+		public String[][] tableroH = new String[15][15];
+		public void genenrartableroH() {
+			for (int i = 0; i < 15; i++) {
+				for (int j = 0; j < 15; j++) {
+					tableroH[i][j] = "[-]"; // inicializar y dejar en valor blanc en ves que esten null.
+			}
+			}
 		}
+		
+	public static String victroria() {
+			return "FELICIDADES\n"
+					+ "HAS DERROTADO A LA TROPA PKS\n"
+					+ "LA ANARQUIA SE EXIENDE POR OSCARILANDIA FINALMENTE";		
+		}
+		
 		
 
 	
-	private static void ubicarKromis(String[][] tablero) {
+	public static void ubicarKromis(String[][] tablero) {
         for (int i = 0; i < 3; i++) {
             int row = random.nextInt(15 - 3 + 1);
             int col = random.nextInt(15);
@@ -92,7 +63,7 @@ public class Tablero {
         }
     }
 	
-	private static void ubicarCaguano(String[][] tablero) {
+	public static void ubicarCaguano(String[][] tablero) {
         for (int i = 0; i < 5; i++) {
             int row = random.nextInt(15);
             int col = random.nextInt(15 - 2 + 1);
@@ -108,7 +79,7 @@ public class Tablero {
         }
     }
 	
-	private static void ubicarTrupalla(String[][] tablero) {
+	public static void ubicarTrupalla(String[][] tablero) {
         for (int i = 0; i < 10; i++) {
             int row = random.nextInt(15);
             int col = random.nextInt(15);
@@ -144,60 +115,82 @@ public class Tablero {
 	    int y;
 	    boolean lanzamientoOk = false;
 	    while (lanzamientoOk == false) {
-	    	System.out.println("Ingrese la columna donde desea lanzar el huevo (de 0 a 14):");
-		    x = scanner.nextInt();
-		    if (x <= 14) {
-		    	System.out.println("Posicion incorrecta. Debe de ser de 0 a 14");
-		    }
-		    System.out.println("Ingrese la fila donde desea lanzar el huevo (de 0 a 14):");
-		    y = scanner.nextInt();   
-		    if (y >= 14) {
-		    	System.out.println("Posicion incorrecta. Debe de ser de 0 a 14");
-		    }
-		    scanner.nextLine();
-		    lanzamientoOk = true;
-		    for (int i = 0; i < 10; i++) {
-	            for (int j = 0; j < 1; j++) {
-	                tableroH[x + j][y] = "["+ANSI_YELLOW+"H"+ANSI_RESET+"]";
-	            }
-		    }  
-		    Huevo huevo = new Huevo(x,y);
-		    huevos.add(huevo);
-		    if(tablero[x][y] != "[-]") {
-		    	if (tablero[x][y] == "["+ANSI_CYAN+"T"+ANSI_RESET+"]" ) {
-		    		System.out.println("Le diste a una Trupalla y te la echaste! - 1 punto");
-		    		tableroH[x][y] = "["+ANSI_RED+"H"+ANSI_RESET+"]";
-		    		puntaje++;
-		    	}
-		    	if (tablero[x][y] == "["+ANSI_BLUE+"C"+ANSI_RESET+"]" ) {
-		    		System.out.println("Le diste a un Caguano! - 2 punto");
-		    		tableroH[x][y] = "["+ANSI_RED+"H"+ANSI_RESET+"]";
-		    		puntaje+=2;
-		    	}
-		    	if (tablero[x][y] == "["+ANSI_GREEN+"K"+ANSI_RESET+"]" ) {
-		    		System.out.println("Le diste a una Kromi! - 3 punto");
-		    		tableroH[x][y] = "["+ANSI_RED+"H"+ANSI_RESET+"]";
-		    		puntaje+=3;
-		    	}
-		        mostrarTableroH(tableroH);  
-		    }  else {
-		    	System.out.println(ANSI_CYAN+"El huevo ha impactado simplemente contra el asfalto.... ");
-		    	System.out.println("...puedes escuchar las burlas de los PKS"+ANSI_RESET);
-		    	mostrarTableroH(tableroH);	
-		    }
+	    	try {
+	    		x = askX();
+	    		y = askY();
+	    		if (tableroH[x][y].equals("["+ANSI_YELLOW+"H"+ANSI_RESET+"]") || tableroH[x][y].equals("["+ANSI_RED+"H"+ANSI_RESET+"]")) {
+	    			System.out.println("Ya has lanzado un huevo en esta posicion");
+	    			break;
+	    		}
+	    		scanner.nextLine();
+	    		lanzamientoOk = true;
+	    		for (int i = 0; i < 10; i++) {
+	    			for (int j = 0; j < 1; j++) {
+	    				tableroH[x + j][y] = "["+ANSI_YELLOW+"H"+ANSI_RESET+"]";
+	    			}
+	    		}  
+	    		Huevo huevo = new Huevo(x,y);
+	    		huevos.add(huevo);
+	    		if(tablero[x][y] != "[-]") {
+	    			if (tablero[x][y] == "["+ANSI_CYAN+"T"+ANSI_RESET+"]" ) {
+	    				System.out.println(ANSI_GREEN+"Le diste a una Trupalla y te la echaste! - 1 punto"+ANSI_RESET);
+	    				tableroH[x][y] = "["+ANSI_RED+"H"+ANSI_RESET+"]";
+	    				puntaje++;
+	    			}
+	    			if (tablero[x][y] == "["+ANSI_BLUE+"C"+ANSI_RESET+"]" ) {
+	    				System.out.println(ANSI_GREEN+"Le diste a un Caguano! - 2 punto"+ANSI_RESET);
+	    				tableroH[x][y] = "["+ANSI_RED+"H"+ANSI_RESET+"]";
+	    				puntaje+=2;
+	    			}
+	    			if (tablero[x][y] == "["+ANSI_GREEN+"K"+ANSI_RESET+"]" ) {
+	    				System.out.println(ANSI_GREEN+"Le diste a una Kromi! - 3 punto"+ANSI_RESET);
+	    				tableroH[x][y] = "["+ANSI_RED+"H"+ANSI_RESET+"]";
+	    				puntaje+=3;
+	    			}
+	    			mostrarTableroH(tableroH);
+	    		}  else {
+	    			System.out.println(ANSI_CYAN+"El huevo ha impactado simplemente contra el asfalto.... ");
+	    			System.out.println("...puedes escuchar las burlas de los PKS"+ANSI_RESET);
+	    			mostrarTableroH(tableroH);	
+	    		}
+	    	}
+	    	catch (Exception e) {
+	    		System.out.println("Posicion incorrecta. Debe de ser de 0 a 14");
+	    	}
+
 	    }
 	}
 	
 	public static void mostrarTableroPks(String [][] tablero) {
-		System.out.println(ANSI_YELLOW+"| 0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 |"+ANSI_RESET);
-		for (int x=0; x < tablero.length; x++) {
-			  System.out.print("|");
-			  for (int y=0; y < tablero[x].length; y++) {
-			    System.out.print (tablero[x][y]);
-			    if (y!=tablero[x].length-1) System.out.print("");
-			  }
-			  System.out.println("|"+ANSI_YELLOW+x+ANSI_RESET);
+		boolean consentir = false;
+		while (consentir == false) {
+			System.out.print("Ver el tablero te costara 10pts, desea continuar? (y/n)");
+			String ans = scanner.nextLine();
+			if(ans.matches("^[yn]$")){
+				if (ans.equalsIgnoreCase("y")) {
+					System.out.print("tramposo.... supongo te gano la angustia de no darle a nada...aqui tienes");
+					System.out.print(ANSI_PURPLE+"\t10 pts han sido restados"+ANSI_RESET);
+					puntaje -=10; System.out.println(ANSI_RED+"-10"+ANSI_RESET);
+					System.out.println(ANSI_YELLOW+"| 0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 |"+ANSI_RESET);
+					for (int x=0; x < tablero.length; x++) {
+						System.out.print("|");
+						for (int y=0; y < tablero[x].length; y++) {
+							System.out.print (tablero[x][y]);
+							if (y!=tablero[x].length-1) System.out.print("");
+						}
+						System.out.println("|"+ANSI_YELLOW+x+ANSI_RESET);
+						consentir = true;
+					}
+				} else {
+					if (ans.equalsIgnoreCase("n")) {
+						System.out.println("Bien me parece, sin hacer trampas");
+						consentir = true;
+						break;
+					}
+				}
 			}
+		}
+		
 	}
 	
 	public static void mostrarTableroH(String[][] tableroH) {
@@ -213,35 +206,40 @@ public class Tablero {
 	}
 	
 	public static void takeALuckyShot(String tablero [][], String tableroH [][]) {
-	    
-		    int x = random.nextInt(15);
-		    int y = random.nextInt(15);
-		    tableroH[x][y] = "["+ANSI_YELLOW+"H"+ANSI_RESET+"]";
-		    Huevo huevo = new Huevo(x,y);
-		    huevos.add(huevo);
-		    if(tablero[x][y] != "[-]") {
-		    	if (tablero[x][y] == "["+ANSI_CYAN+"T"+ANSI_RESET+"]" ) {
-		    		System.out.println("Le diste a una Trupalla y te la echaste! - 1 punto");
-		    		tableroH[x][y] = "["+ANSI_RED+"H"+ANSI_RESET+"]";
-		    		puntaje++;
-		    	}
-		    	if (tablero[x][y] == "["+ANSI_BLUE+"C"+ANSI_RESET+"]" ) {
-		    		System.out.println("Le diste a un Caguano! - 2 punto");
-		    		tableroH[x][y] = "["+ANSI_RED+"H"+ANSI_RESET+"]";
-		    		puntaje+=2;
-		    	}
-		    	if (tablero[x][y] == "["+ANSI_GREEN+"K"+ANSI_RESET+"]" ) {
-		    		System.out.println("Le diste a una Kromi! - 3 punto");
-		    		tableroH[x][y] = "["+ANSI_RED+"H"+ANSI_RESET+"]";
-		    		puntaje+=3;
-		    	}
-		        mostrarTableroH(tableroH);  
-		    }  else {
-		    	System.out.println(ANSI_CYAN+"El huevo ha impactado simplemente contra el asfalto.... ");
-		    	System.out.println("...puedes escuchar las burlas de los PKS"+ANSI_RESET);
-		    	mostrarTableroH(tableroH);	
-		    }
-	    }
+		int x = 0;
+		int y= 0;
+		while(tableroH[x][y].equals("["+ANSI_YELLOW+"H"+ANSI_RESET+"]") || tableroH[x][y].equals("["+ANSI_RED+"H"+ANSI_RESET+"]"))
+			x = random.nextInt(15-1);
+		y = random.nextInt(15-1);
+		tableroH[x][y] = "["+ANSI_YELLOW+"H"+ANSI_RESET+"]";
+		Huevo huevo = new Huevo(x,y);
+		huevos.add(huevo);
+		if(tablero[x][y] != "[-]") {
+			if (tablero[x][y] == "["+ANSI_CYAN+"T"+ANSI_RESET+"]" ) {
+				System.out.println(ANSI_GREEN+"Le diste a una Trupalla y te la echaste! - 1 punto"+ANSI_RESET);
+				tableroH[x][y] = "["+ANSI_RED+"H"+ANSI_RESET+"]";
+				puntaje++;
+			}
+			if (tablero[x][y] == "["+ANSI_BLUE+"C"+ANSI_RESET+"]" ) {
+				System.out.println(ANSI_GREEN+"Le diste a un Caguano! - 2 punto"+ANSI_RESET);
+				tableroH[x][y] = "["+ANSI_RED+"H"+ANSI_RESET+"]";
+				puntaje+=2;
+			}
+			if (tablero[x][y] == "["+ANSI_GREEN+"K"+ANSI_RESET+"]" ) {
+				System.out.println(ANSI_GREEN+"Le diste a una Kromi! - 3 punto"+ANSI_RESET);
+				tableroH[x][y] = "["+ANSI_RED+"H"+ANSI_RESET+"]";
+				puntaje+=3;
+			}
+			mostrarTableroH(tableroH);  
+			
+		}  else {
+			System.out.println(ANSI_CYAN+"El huevo ha impactado simplemente contra el asfalto.... ");
+			System.out.println("...puedes escuchar las burlas de los PKS"+ANSI_RESET);
+			mostrarTableroH(tableroH);	
+		}
+	}
+
+
 	
 	public static void mostrarDatos() {
 		int i = 1;
@@ -272,6 +270,47 @@ public class Tablero {
 		}
 		
 	}
+	
+	public static int askX() {
+	    int x=-1;
+	    boolean preguntaOk = false;
+	    while (!preguntaOk) {
+	        try {
+	            System.out.println("Ingrese la fila (" + ANSI_YELLOW + "--" + ANSI_RESET + ") donde desea lanzar el huevo (de 0 a 14):");
+	            String ax = scanner.nextLine();
+	            if (!ax.matches("^(1?[0-4]|[0-9])$")) {
+	                throw new Exception();
+	            }
+	            x = Integer.parseInt(ax);
+	            preguntaOk = true;
+	        } catch (Exception e) {
+	            System.out.println("Debe ser un número de 0 a 14, sin letras ni caracteres.");
+	            continue;
+	        }
+	    }
+	    return x;
+	}
+	public static int askY() {
+	    int y=-1;
+	    boolean preguntaOk = false;
+	    while (!preguntaOk) {
+	        try {
+	            System.out.println("Ingrese la fila (" + ANSI_YELLOW + "--" + ANSI_RESET + ") donde desea lanzar el huevo (de 0 a 14):");
+	            String ay = scanner.nextLine();
+	            if (!ay.matches("^(1?[0-4]|[0-9])$")) {
+	                throw new Exception();
+	            }
+	            y = Integer.parseInt(ay);
+	            preguntaOk = true;
+	        } catch (Exception e) {
+	            System.out.println("Debe ser un número de 0 a 14, sin letras ni caracteres.");
+	            continue;
+	        }
+	    }
+	    return y;
+	}
+	
+
 	
 	public static final String ANSI_WHITE = "\u001B[37m";
 	public static final String ANSI_PURPLE = "\u001B[35m";
